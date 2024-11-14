fa100374687d2c02668b2288a0aa020369cd3993
kylelirette93 <162719550+kylelirette93@users.noreply.github.com>
2024-11-14 13:09:20 -0400

    Added different reflection based on paddle hit

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,10 +7,15 @@ public class Ball : MonoBehaviour
 {
     float speed = 5f;
     Vector3 desiredDirection;
+    Vector3 diagonalLeft;
+    Vector3 diagonalRight;
 
     private void Start()
     {
         desiredDirection = -transform.up;
+        diagonalLeft = transform.up + transform.right;
+        diagonalRight = transform.up - transform.right;
+
     }
 
 
@@ -18,11 +23,27 @@ public class Ball : MonoBehaviour
     {
         transform.position += desiredDirection * speed * Time.deltaTime;
 
+       
         RaycastHit hit;
         Physics.Raycast(transform.position, desiredDirection, out hit, 0.2f);
 
         if (hit.collider != null)
         {
+            if (hit.collider.CompareTag("Paddle"))
+            {
+                float paddleCenterX = hit.collider.transform.position.x;
+                float ballX = transform.position.x;
+
+                if (ballX < paddleCenterX)  
+                {
+                    desiredDirection = Vector3.Reflect(diagonalRight, hit.normal);
+                }
+                else  
+                {
+                    desiredDirection = Vector3.Reflect(diagonalLeft, hit.normal);
+                }
+
+            }
             desiredDirection = Vector3.Reflect(desiredDirection, hit.normal);
         }
     }
